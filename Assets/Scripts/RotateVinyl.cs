75002a93c79fd3c81b741d527e075596cff233f2
Pekka Nisula <pekka.nisula@resistancegames.com>
2020-02-01 23:45:32 +0200

    add DJ stuff

---
+++ Assets/Scripts/RotateVinyl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateVinyl : MonoBehaviour
+{    
+    // Update is called once per frame
+    void Update()
+    {        
+        this.transform.Rotate(0.0f, 10*Time.deltaTime, 0.0f, Space.World);
+    }
+}
